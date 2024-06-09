0527806730ea976ea0677ebefb1223ff871442c6
Umar <umarsyed703@gmail.com>
2024-06-08 20:45:12 -0400

    routing done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "@/components/ui/button";
-
+import Navbar from "Navbar/Navbar";
+import HealthServices from "Health_Services/healthServices";
+import Donate from "Donate/donate";
+import OurStory from "About_Us/ourStory";
+import Volunteers from "About_Us/volunteers";
+import Partners from "About_Us/partners";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Button>welcome</Button>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Button>Welcome</Button>} />
+        <Route path="/health-services" element={<HealthServices />} />
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/our-story" element={<OurStory />} />
+        <Route path="/volunteers" element={<Volunteers />} />
+        <Route path="/partners" element={<Partners />} />
+      </Routes>
     </div>
   );
 }
