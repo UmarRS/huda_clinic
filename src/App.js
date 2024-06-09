486dce3a894465a7cd627cac4a8c804afc902153
Umar <umarsyed703@gmail.com>
2024-06-08 20:56:44 -0400

    navbar with routing

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import Donate from "Donate/donate";
 import OurStory from "About_Us/ourStory";
 import Volunteers from "About_Us/volunteers";
 import Partners from "About_Us/partners";
+import Home from "Home/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Button>Welcome</Button>} />
-        <Route path="/health-services" element={<HealthServices />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/healthservices" element={<HealthServices />} />
         <Route path="/donate" element={<Donate />} />
-        <Route path="/our-story" element={<OurStory />} />
+        <Route path="/ourstory" element={<OurStory />} />
         <Route path="/volunteers" element={<Volunteers />} />
         <Route path="/partners" element={<Partners />} />
       </Routes>
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,5 +1,27 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
-export default Navbar;
+function NavigationButtons() {
+  return (
+    <div>
+      {/* You can delete this from here, this is just here to help */}
+      <div className="flex justify-around p-5">
+        <Link to="/ourstory" className="text-white font-bold py-2 px-4 ">
+          <Button className="rounded-xl">About Us</Button>
+        </Link>
+        <Link to="/healthservices" className=" text-white font-bold py-2 px-4">
+          <Button className="rounded-xl">Health Services</Button>
+        </Link>
+        <Link to="/donate" className=" text-white font-bold py-2 px-4">
+          <Button className="rounded-xl">Donate</Button>
+        </Link>
+        <Link to="/volunteers" className=" text-white font-bold py-2 px-4 ">
+          <Button className="rounded-xl">Volunteers</Button>
+        </Link>
+      </div>
+      {/* To here */}
+    </div>
+  );
+}
+
+export default NavigationButtons;
